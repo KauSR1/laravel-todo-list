aa7cbd1d4af60e1cf5fcadd02d0371545ad38848
Kauan Silva <kausilva.r3@gmail.com>
2026-08-05 20:56:57 -0300

    feat(database): create tasks table migration and update user factory

---
+++ database/migrations/2026_08_05_224627_create_tasks_tables.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks_table', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('priority')->default(0);
+            $table->date('date_limited');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks_table');
+    }
+};
